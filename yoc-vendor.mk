685a04a370cee49a867613f1fd3c251a7cb815b3
deskjet390 <deskjet390@gmail.com>
2023-10-18 08:51:41 -0500

    Update yoc-vendor.mk
    change to onn

--- yoc-vendor.mk
+++ yoc-vendor.mk
@@ -6,29 +6,29 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/askey/yoc
 
 PRODUCT_COPY_FILES += \
-    vendor/askey/dopinder/proprietary/vendor/lib/liboemcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib/liboemcrypto.so \
-    vendor/askey/dopinder/proprietary/vendor/lib/libsecmem.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsecmem.so \
-    vendor/askey/dopinder/proprietary/vendor/lib/teetz/00d1ca22-1764-4e35-90aa5b8c12630764.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/00d1ca22-1764-4e35-90aa5b8c12630764.ta \
-    vendor/askey/dopinder/proprietary/vendor/lib/teetz/2c1a33c0-44cc-11e5-bc3b0002a5d5c51b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/2c1a33c0-44cc-11e5-bc3b0002a5d5c51b.ta \
-    vendor/askey/dopinder/proprietary/vendor/lib/teetz/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta \
-    vendor/askey/dopinder/proprietary/vendor/lib/teetz/8efb1e1c-37e5-4326-a5d68c33726c7d57.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/8efb1e1c-37e5-4326-a5d68c33726c7d57.ta \
-    vendor/askey/dopinder/proprietary/vendor/lib/teetz/9a04f079-9840-4286-ab92e65be0885f95.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/9a04f079-9840-4286-ab92e65be0885f95.ta \
-    vendor/askey/dopinder/proprietary/vendor/lib/teetz/e043cde0-61d0-11e5-9c260002a5d5c51b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e043cde0-61d0-11e5-9c260002a5d5c51b.ta \
-    vendor/askey/dopinder/proprietary/vendor/lib/teetz/e92a43ab-b4c8-4450-aa12b1516259613b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e92a43ab-b4c8-4450-aa12b1516259613b.ta \
-    vendor/askey/dopinder/proprietary/vendor/lib/teetz/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta \
-    vendor/askey/dopinder/proprietary/vendor/usr/idc/Vendor_0508_Product_0110.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_0508_Product_0110.idc \
-    vendor/askey/dopinder/proprietary/vendor/usr/idc/Vendor_0957_Product_0004.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_0957_Product_0004.idc \
-    vendor/askey/dopinder/proprietary/vendor/usr/idc/Vendor_1d5a_Product_c082.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_1d5a_Product_c082.idc \
-    vendor/askey/dopinder/proprietary/vendor/usr/idc/Vendor_248a_Product_8266.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_248a_Product_8266.idc \
-    vendor/askey/dopinder/proprietary/vendor/usr/idc/Vendor_7545_Product_0171.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_7545_Product_0171.idc \
-    vendor/askey/dopinder/proprietary/vendor/usr/idc/Vendor_7545_Product_0180.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_7545_Product_0180.idc \
-    vendor/askey/dopinder/proprietary/vendor/usr/idc/virtual-remote.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/virtual-remote.idc \
-    vendor/askey/dopinder/proprietary/vendor/usr/idc/virtual-search.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/virtual-search.idc \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_0001_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0001_Product_0001.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_005d_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_005d_Product_0001.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_005d_Product_0002.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_005d_Product_0002.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_0957_Product_0004.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0957_Product_0004.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_1d5a_Product_c081.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_1d5a_Product_c081.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_1d5a_Product_c082.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_1d5a_Product_c082.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_248a_Product_8266.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_248a_Product_8266.kl \
-    vendor/askey/dopinder/proprietary/vendor/usr/keylayout/Vendor_7545_Product_0171.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_7545_Product_0171.kl
+    vendor/onn/yoc/proprietary/vendor/lib/liboemcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib/liboemcrypto.so \
+    vendor/onn/yoc/proprietary/vendor/lib/libsecmem.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsecmem.so \
+    vendor/onn/yoc/proprietary/vendor/lib/teetz/00d1ca22-1764-4e35-90aa5b8c12630764.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/00d1ca22-1764-4e35-90aa5b8c12630764.ta \
+    vendor/onn/yoc/proprietary/vendor/lib/teetz/2c1a33c0-44cc-11e5-bc3b0002a5d5c51b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/2c1a33c0-44cc-11e5-bc3b0002a5d5c51b.ta \
+    vendor/onn/yoc/proprietary/vendor/lib/teetz/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta \
+    vendor/onn/yoc/proprietary/vendor/lib/teetz/8efb1e1c-37e5-4326-a5d68c33726c7d57.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/8efb1e1c-37e5-4326-a5d68c33726c7d57.ta \
+    vendor/onn/yoc/proprietary/vendor/lib/teetz/9a04f079-9840-4286-ab92e65be0885f95.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/9a04f079-9840-4286-ab92e65be0885f95.ta \
+    vendor/onn/yoc/proprietary/vendor/lib/teetz/e043cde0-61d0-11e5-9c260002a5d5c51b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e043cde0-61d0-11e5-9c260002a5d5c51b.ta \
+    vendor/onn/yoc/proprietary/vendor/lib/teetz/e92a43ab-b4c8-4450-aa12b1516259613b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e92a43ab-b4c8-4450-aa12b1516259613b.ta \
+    vendor/onn/yoc/proprietary/vendor/lib/teetz/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/ff2a4bea-ef6d-11e6-89ccd4ae52a7b3b3.ta \
+    vendor/onn/yoc/proprietary/vendor/usr/idc/Vendor_0508_Product_0110.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_0508_Product_0110.idc \
+    vendor/onn/yoc/proprietary/vendor/usr/idc/Vendor_0957_Product_0004.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_0957_Product_0004.idc \
+    vendor/onn/yoc/proprietary/vendor/usr/idc/Vendor_1d5a_Product_c082.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_1d5a_Product_c082.idc \
+    vendor/onn/yoc/proprietary/vendor/usr/idc/Vendor_248a_Product_8266.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_248a_Product_8266.idc \
+    vendor/onn/yoc/proprietary/vendor/usr/idc/Vendor_7545_Product_0171.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_7545_Product_0171.idc \
+    vendor/onn/yoc/proprietary/vendor/usr/idc/Vendor_7545_Product_0180.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Vendor_7545_Product_0180.idc \
+    vendor/onn/yoc/proprietary/vendor/usr/idc/virtual-remote.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/virtual-remote.idc \
+    vendor/onn/yoc/proprietary/vendor/usr/idc/virtual-search.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/virtual-search.idc \
+    vendor/onn/yoc/proprietary/vendor/usr/keylayout/Vendor_0001_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0001_Product_0001.kl \
+    vendor/onn/yoc/proprietary/vendor/usr/keylayout/Vendor_005d_Product_0001.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_005d_Product_0001.kl \
+    vendor/onn/yoc/proprietary/vendor/usr/keylayout/Vendor_005d_Product_0002.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_005d_Product_0002.kl \
+    vendor/onn/yoc/proprietary/vendor/usr/keylayout/Vendor_0957_Product_0004.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_0957_Product_0004.kl \
+    vendor/onn/yoc/proprietary/vendor/usr/keylayout/Vendor_1d5a_Product_c081.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_1d5a_Product_c081.kl \
+    vendor/onn/yoc/proprietary/vendor/usr/keylayout/Vendor_1d5a_Product_c082.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_1d5a_Product_c082.kl \
+    vendor/onn/yoc/proprietary/vendor/usr/keylayout/Vendor_248a_Product_8266.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_248a_Product_8266.kl \
+    vendor/onn/yoc/proprietary/vendor/usr/keylayout/Vendor_7545_Product_0171.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Vendor_7545_Product_0171.kl
